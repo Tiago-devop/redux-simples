610954348779cf2d7ced6d1c1cb9ddcdd529b067
tiagoramosvasconcelos.work@gmail.com <tiagodev@protonmail.com>
2020-12-22 10:20:54 -0300

    componente card

--- src/App.js
+++ src/App.js
@@ -7,14 +7,20 @@ function App() {
     <div className="App">
       <h1>Exercício React-Redux (Simples)</h1>
       <div className="linha">
-        <Card />
-        <Card />
-        <Card />
+        <Card title="Card 1" red>
+          X
+        </Card>
+        <Card title="Card 2" green>
+          X
+        </Card>
       </div>
       <div className="linha">
-        <Card />
-        <Card />
-        <Card />
+        <Card title="Card 3" blue>
+          y
+        </Card>
+        <Card title="Card 4" purple>
+          X
+        </Card>
       </div>
     </div>
   );
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,6 +1,21 @@
 import './Card.css';
 import React from 'react';
 
-export default () => {
-  return <div>Card</div>;
+function getColor(props) {
+  if (props.red) return 'Red';
+  if (props.green) return 'Green';
+  if (props.purple) return 'Purple';
+  if (props.blue) return 'Blue';
+  return '';
+}
+
+export default (props) => {
+  return (
+    <div className={`Card ${getColor(props)}`}>
+      <div className="Header">
+        <span className="Title">{props.title}</span>
+      </div>
+      <div className="Content">{props.children}</div>
+    </div>
+  );
 };
