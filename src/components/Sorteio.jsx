6e38237abf8aff88f142e79325466c618f3a2110
tiagoramosvasconcelos.work@gmail.com <tiagodev@protonmail.com>
2020-12-26 17:25:51 -0300

    conectando componentes com redux

--- src/components/Sorteio.jsx
+++ src/components/Sorteio.jsx
@@ -1,15 +1,28 @@
 import React from 'react';
+import { connect } from 'react-redux';
+
 import Card from './Card';
 
-export default () => {
+function Sorteio(props) {
+  const { min, max } = props;
+  const randomNumber = parseInt(Math.random() * (max - min), 10) + min;
   return (
     <Card title="Sorteio de um Número" purple>
       <div>
         <span>
           <span>Resultado:</span>
-          <strong>{10}</strong>
+          <strong>{randomNumber}</strong>
         </span>
       </div>
     </Card>
   );
-};
+}
+
+function mapStateProps(state) {
+  return {
+    min: state.numeros.min,
+    max: state.numeros.max,
+  };
+}
+
+export default connect(mapStateProps)(Sorteio);
