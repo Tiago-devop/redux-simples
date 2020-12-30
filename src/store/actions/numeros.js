7cfb60310489a770083705e271781d1c34ff4572
Tiago-devop <tiagodev@protonmail.com>
2020-12-30 08:07:47 -0300

    contantes para os tipos das actions

--- src/store/actions/numeros.js
+++ src/store/actions/numeros.js
@@ -1,15 +1,17 @@
+import { NUM_MIN_ALTERADO, NUM_MAX_ALTERADO } from './actionTypes';
+
 // Action Creator
 // eslint-disable-next-line import/prefer-default-export
 export function alterarNumeroMinimo(novoNumero) {
   return {
-    type: 'NUM_MIN_ALTERADO',
+    type: NUM_MIN_ALTERADO,
     payload: novoNumero,
   };
 }
 
 export function alterarNumeroMaximo(novoNumero) {
   return {
-    type: 'NUM_MAX_ALTERADO',
+    type: NUM_MAX_ALTERADO,
     payload: novoNumero,
   };
 }
