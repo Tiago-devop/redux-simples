d1f2398ff8aacecb8c503314ce55440a73536d3a
Tiago-devop <tiagodev@protonmail.com>
2020-12-30 07:55:31 -0300

    refatorando reducer

---
+++ src/store/reducers/numeros.js
@@ -0,0 +1,23 @@
+const initialState = {
+  min: 1,
+  max: 10,
+};
+
+// eslint-disable-next-line func-names
+export default function (state = initialState, action) {
+  switch (action.type) {
+    case 'NUM_MIN_ALTERADO':
+      return {
+        ...state,
+        min: action.payload,
+      };
+    case 'NUM_MAX_ALTERADO':
+      return {
+        ...state,
+        max: action.payload,
+      };
+
+    default:
+      return state;
+  }
+}
--- src/store/storeConfig.js
+++ src/store/storeConfig.js
@@ -2,31 +2,10 @@
 /* eslint-disable func-names */
 import { createStore, combineReducers } from 'redux';
 
+import numerosReducer from './reducers/numeros';
+
 const reducers = combineReducers({
-  numeros: function (state, action) {
-    switch (action.type) {
-      case 'NUM_MIN_ALTERADO':
-        return {
-          ...state,
-          min: action.payload,
-        };
-      case 'NUM_MAX_ALTERADO':
-        return {
-          ...state,
-          max: action.payload,
-        };
-      default:
-        return {
-          min: 7,
-          max: 31,
-        };
-    }
-  },
-  nomes: function (state, action) {
-    console.log('Reducer Numeros...');
-    console.log(state, '', action);
-    return ['Maria', 'Juca', 'Carlos'];
-  },
+  numeros: numerosReducer,
 });
 
 function storeConfig() {
